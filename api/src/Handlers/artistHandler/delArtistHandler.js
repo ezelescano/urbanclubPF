ba6fb9b6b4bcd8549cd0cde2cb4b2250812382eb
ppalmav <ppalmav@users.noreply.github.com>
2023-04-25 17:59:53 -0400

    Pablo_Dev: ruta delete con controller y handler

---
+++ api/src/Handlers/artistHandler/delArtistHandler.js
@@ -0,0 +1,18 @@
+const { delArtist } = require("../../Controllers/artistControllers/delArtist");
+
+const delArtistHandler = async function (req,res){
+  
+ 
+  
+    // res.send("Artist is deleted");
+
+    const { id } = req.params;
+    try {
+      const deleted = await delArtist(id);
+      res.status(200).send("Artist is deleted");
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  };
+
+module.exports = {delArtistHandler}
--- api/src/routes/artistRouter.js
+++ api/src/routes/artistRouter.js
@@ -1,12 +1,14 @@
 const {Router} = require("express");
 const { getAllArtistHandler } = require("../Handlers/artistHandler/getAllArtistHandlers");
 const {getArtistById} = require("../Handlers/artistHandler/getArtistById")
+const {delArtistHandler} = require("../Handlers/artistHandler/delArtistHandler")
 // const {postArtistHandler} = require("../Handlers/postArtistHandler")
 const artistRouter = Router();
 
 
 artistRouter.get("/", getAllArtistHandler);
 artistRouter.get("/:id", getArtistById);
+artistRouter.delete("/:id", delArtistHandler);
 
 
 
