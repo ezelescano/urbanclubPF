37b6339640d932747291d5c57b6dea5ab7e7d628
Ezequiel <ezequiel.lesca@gmail.com>
2023-04-26 18:07:34 -0300

    Eze_Dev eventDelete hecho, falta probar Ezequiel

---
+++ api/src/Handlers/eventHandlar/delEventHandler.js
@@ -0,0 +1,15 @@
+const deleteEvent = require("../../Controllers/eventController/delEventController");
+
+
+const delEventHandler = async (req, res) => {
+    const { id } = req.params;
+    try {
+            const result = await deleteEvent(id);
+            res.status(204).json({ message: 'The event was deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'An error occurred while deleting the user' });
+
+    }
+}
+
+module.exports= delEventHandler;
--- api/src/routes/eventRouter.js
+++ api/src/routes/eventRouter.js
@@ -1,7 +1,9 @@
 const {Router} = require("express");
 const eventRouter = Router();
 const {getEventsHandler} = require("../Handlers/eventHandlar");
+const delEventHandler = require("../Handlers/eventHandlar/delEventHandler");
 
 eventRouter.get("/", getEventsHandler);
+eventRouter.delete("/:id", delEventHandler);
 
 module.exports = eventRouter;
