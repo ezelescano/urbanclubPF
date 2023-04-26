1415f178785cc220e31f76a3bf97ed2520dd6655
Ezequiel <ezequiel.lesca@gmail.com>
2023-04-26 00:39:48 -0300

    Eze_Dev GetAllArtist terminado, falta by name Ezequiel

--- api/src/Controllers/artistControllers/getAllArtist.js
+++ api/src/Controllers/artistControllers/getAllArtist.js
@@ -1,17 +1,46 @@
-const {Artist} = require("../../db");
+const { Sequelize } = require("sequelize");
+const { Artist } = require("../../db");
 
 const getAllArtist = async () => {
-    const artistDB = await Artist.find({
-        attributes: ["id", "name", "lastname", "email", "profilePhoto", "coverPhoto", "password", "nickName", "Country", "city", "ocupation","aboutMe"]
+    const artistDB = await Artist?.findAll({
+        attributes: ["id", "name", "lastname", "email", "profilePhoto", "coverPhoto", "password", "nickName", "Country", "city", "ocupation", "aboutMe"]
     })
-    return artistDB; 
+    return artistDB;
 }
 
 const getArtistByName = async (name) => {
+    const nameSplit = name?.split(" ");
+
+    if (nameSplit.length > 0) {
+        const lowerWords = await nameSplit.map(word => word?.toLowerCase());
+        const finalString = lowerWords.join(" ");
+        const artists = await Artist?.findAll({
+            where: {
+                [Sequelize.Op.or]: [
+                    Sequelize.literal(`lower(concat(name, ' ', lastname)) like '%${finalString}%'`),
+                    { nickname: { [Sequelize.Op.iLike]: `%${finalString}%` } }
+                ]
+            }
+        }
+        );
+        console.log("///soy artist", artists);
+        return artists;
+    } else{
+     const   artist = await Artist.findAll({
+        where :{name: name }
+    }) 
+    return artist;
+    }
+
 
 }
+
+
+
 
 module.exports = {
-    getAllArtist, 
+    getAllArtist,
     getArtistByName
 }
+
+
