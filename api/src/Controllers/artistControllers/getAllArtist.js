48b97c4b885a14f4da993f5e44c42299cd48bce8
Ezequiel <ezequiel.lesca@gmail.com>
2023-04-26 09:44:48 -0300

    Eze_Dev funcion getArtistByName terminada Ezequiel

--- api/src/Controllers/artistControllers/getAllArtist.js
+++ api/src/Controllers/artistControllers/getAllArtist.js
@@ -1,38 +1,37 @@
-const { Sequelize } = require("sequelize");
+const { Op } = require("sequelize");
 const { Artist } = require("../../db");
 
 const getAllArtist = async () => {
     const artistDB = await Artist?.findAll({
-        attributes: ["id", "name", "lastname", "email", "profilePhoto", "coverPhoto", "password", "nickName", "Country", "city", "ocupation", "aboutMe"]
+        attributes: ["name", "lastname", "nickName", "ocupation"]
     })
     return artistDB;
 }
 
 const getArtistByName = async (name) => {
-    const nameSplit = name?.split(" ");
-
-    if (nameSplit.length > 0) {
-        const lowerWords = await nameSplit.map(word => word?.toLowerCase());
-        const finalString = lowerWords.join(" ");
-        const artists = await Artist?.findAll({
-            where: {
-                [Sequelize.Op.or]: [
-                    Sequelize.literal(`lower(concat(name, ' ', lastname)) like '%${finalString}%'`),
-                    { nickname: { [Sequelize.Op.iLike]: `%${finalString}%` } }
-                ]
-            }
-        }
-        );
-        console.log("///soy artist", artists);
-        return artists;
-    } else{
-     const   artist = await Artist.findAll({
-        where :{name: name }
-    }) 
-    return artist;
+    const nameSplit = name.split(" ");
+    const finalWords = [];
+    for(let i = 0; i < nameSplit.length; i++){
+      const word = nameSplit[i];
+      const firstLetter = word.charAt(0).toUpperCase();
+      const restLetters = word.slice(1).toLowerCase();
+      const finalWord = firstLetter + restLetters;
+      finalWords.push(finalWord);
     }
-
-
+    const finalString = finalWords.join(" ");
+    console.log("//soy el finalstring", finalString);
+    const dataBaseByName = await Artist.findAll({ 
+    where: {
+    [Op.or]: [
+        { name: { [Op.iLike]: `%${finalString}%` } },
+        { nickName: { [Op.iLike]: `%${finalString}%` } },
+        { lastname: {[Op.iLike]: `%${finalString}%`}
+        }  
+    ]
+    },
+    
+     });
+     return dataBaseByName;
 }
 
 
