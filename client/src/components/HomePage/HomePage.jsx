c7c939dc98aa4a53e182db4907cdfdc7e0faaa31
eudesmieres <eumieres@gmail.com>
2023-04-26 12:27:26 -0300

    Carousel,Home Eu_Dev

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Carousel from '../Carousel/Carousel.jsx';
+import style from '../HomePage/Home.module.css';
+import img_banner1 from "../../imagenes/mago3.jpg";
+import img_banner2 from "../../imagenes/musico1.jpg";
+import img_banner3 from "../../imagenes/musico2.jpg";
 
 const HomePage = () => {
+  const images = [img_banner1, img_banner2, img_banner3];
+
   return (
-    <div>Está es la HomePage</div>
+    <div className={style.main}>
+      <div>
+      <Carousel images={images} autoPlay={false}  showButtons={false}/>
+      </div>
+
+      <h1>Está es la HomePage</h1>
+      
+      </div>
   )
 }
 
