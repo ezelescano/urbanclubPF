0b765fbd308acd9112e5090114e298f5153a2535
John Jaramillo <jaramillor03@gmail.com>
2023-04-27 11:10:45 -0500

    Alex_Dev Adelanto profile,creacion profileEdit y route  components/profile App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
 import Profile from "./components/Profile/Profile";
+import ProfileEdit from "./components/ProfileEdit/ProfileEdit";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
 import Navbar from "./components/Navbar/Navbar";
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/profileEdit" element={<ProfileEdit />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import './profile.css'
 import React from "react";
+import { NavLink } from "react-router-dom"
 
 const Profile = () => {
   const usuario = {
@@ -11,39 +12,94 @@ const Profile = () => {
     nickName: "Oscar sin Etiquetas",
     country:"Argentina",
     city:"Buenos aires",
-    ocupation:"Delincuente",
+    ocupation:["Delincuente", "Mago", "Artista"],
     aboutMe:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe enim animi nobis, tempora, facere ex dolorem magnam corporis excepturi nihil voluptates inventore perferendis, laboriosam velit. Facilis praesentium distinctio autem recusandae.",
     followers:3,
-    verified: true
+    verified: false,
+    likes: 46,
+   
+    links: [{
+      youtube: "https://www.youtube.com/el perfil",
+      twitter: "https://twitter.com/"
+    }
+    ],
+    eventos: [{
+      
+    }]
+    
   }
 
   return (
-    <div>
+    <div className='container'>
       <div>
         <img  className="portada-profile" src={usuario.coverPhoto} alt="no se jaja" />
 
-        <div className='foto-nombre'>
-          <img className='foto-profile' src={usuario.profilePhoto} alt='no se jaja x2' />
-          <div className='info-profile'>
-            <div className='nombre'>
-              <h1>Oscar sin Etiquetas</h1>
-              {/* para saber si es verificado funcion aun no implementada */}
-              {usuario.verified &&
-              <div className='verificado-div'>
-                <img className='verificado' src='https://static.vecteezy.com/system/resources/previews/014/296/309/non_2x/blue-verified-social-media-account-icon-approved-profile-sign-illustration-vector.jpg' alt='verificado paa' />
-                <p>Artista verificado</p>
+        <div className='prim-profile'>
+          <div className='foto-nombre'>
+              <img className='foto-profile' src={usuario.profilePhoto} alt='no se jaja x2' />   
+              <div className='nombre'>
+                <h1>{usuario.nickName}</h1>
+                {/* para saber si es verificado funcion aun no implementada */}
+                {usuario.verified &&
+                  <img className='verificado' src='https://static.vecteezy.com/system/resources/previews/014/296/309/non_2x/blue-verified-social-media-account-icon-approved-profile-sign-illustration-vector.jpg' alt='verificado paa' />
+                }
               </div>
-              }
-            </div>
-            <div className='seguidores'>
-            <h4>{usuario.followers} Seguidores</h4>
-            <button className='btn-profile'>Seguir</button>
-            </div>
+              <h3>{usuario.city}, {usuario.country}</h3>
+              <div className='ocupation-container'>
+              {usuario.ocupation?.map(o => {
+                return(
+                  <div className='ocupation'>{o}</div>
+                )
+              })}
+              </div>
+          </div>
+          <div className='stas-btns'>
+            <div className='btns'>
+              <NavLink to="/profileEdit">
+                <button className='btn-profile' >Seguir</button>
+              </NavLink>
+              <NavLink to="/chat">
+                <button className='btn-profile' >Contactar</button>
+              </NavLink>
+              </div> 
+
+              <div className='stas-profile'>
+                <h4>{usuario.followers} Seguidores</h4>
+                <h4>{usuario.followers} Seguidores</h4>
+                <h4>{usuario.likes} Likes</h4>
+              </div>
           </div>
         </div>
+
       </div>
-      <div className='cuerpo'>
-        <p>{usuario.aboutMe}</p>
+      <div className='ab-re'>
+        <div className='aboutme'>
+          <p>{usuario.aboutMe}</p>
+        </div>
+        <div className='redes'>
+          {usuario.links?.map(l => {
+            return(
+              <div className='redes-div'>
+                <h4>Otras redes!!</h4>
+                <div className='container-links'>
+                {l.youtube && <a href={l.youtube} target="_blank" rel="noreferrer noopener"><img className='icon' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYBkoHVpJNDq7zkN5eqjnF31QVBGPb7hloyw&usqp=CAU' alt='ds'/></a>}
+                
+                {l.twitter && <a href={l.twitter} target="_blank" rel="noreferrer noopener"><img className='icon' src='https://upload.wikimedia.org/wikipedia/commons/f/f2/Logo_Twitter.png' alt='ds'/></a>}
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <div className='titulo-ev'>Eventos</div>
+      <div>
+        {/* {usuario.eventos?.map(el => {
+          return (
+            <div>
+              <Cards info={el}/>
+            </div>
+          )
+        })} */}
       </div>
     </div>
   );
