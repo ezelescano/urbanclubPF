7836262f010e2f947ca2b2d93e337b245bb3a087
estiven2111 <estiven.a.martinez@gmail.com>
2023-04-27 16:36:23 -0500

    Est_Dev revision y mod post EAM

--- api/src/app.js
+++ api/src/app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 const fileupload = require("express-fileupload")
+const cors = require("cors")
 const mainRouter = require("./routes/index")
+
 
 
 
 const app = express();
 
 app.use(morgan("dev"));
-
+app.use(cors())
 app.use(express.json());
 app.use(fileupload({
     useTempFiles: true,
