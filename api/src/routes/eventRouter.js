97f178469a36a33b87877bd6ec1cc4a2182e16e3
Ezequiel <ezequiel.lesca@gmail.com>
2023-04-27 18:37:46 -0300

    Eze_Dev update event terminado Ezequiel

--- api/src/routes/eventRouter.js
+++ api/src/routes/eventRouter.js
@@ -7,6 +7,6 @@ const updateEventHandler = require("../Handlers/eventHandlar/updateEventHandler"
 eventRouter.get("/", getEventsHandler);
 eventRouter.delete("/:id", delEventHandler);
 eventRouter.post("/", postEventHandler);
-eventRouter.put("/:id", updateEventHandler);
+eventRouter.put("/update/:id", updateEventHandler);
 
 module.exports = eventRouter;
