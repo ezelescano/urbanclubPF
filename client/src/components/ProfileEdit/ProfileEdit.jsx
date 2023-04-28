ed73f4985ec679775b68c708b85941d61478d6ad
Alan Winkler <alanquimeywinkler@gmail.com>
2023-04-28 14:12:17 -0300

    Alan_Dev Cambios en profilEdit, Alan

--- client/src/components/ProfileEdit/ProfileEdit.jsx
+++ client/src/components/ProfileEdit/ProfileEdit.jsx
@@ -1,49 +1,229 @@
-import React from 'react'
-import { useState } from 'react'
-
-const ProfileEdit = ({inputArtist}) => {
-
-const { name,
-        lastname,
-        email, 
-        profilePhoto, 
-        coverPhoto, 
-        nickName,
-        country,
-        city,
-        ocupation,
-        aboutMe,
-        links} = inputArtist
-
-const [input, setInput] = useState({
-  name: {name},
-  lastname: {lastname},
-  email: {email},
-  profilePhoto: {profilePhoto},
-  coverPhoto: {coverPhoto},
-  nickName: {nickName},
-  country: {country},
-  city: {city},
-  ocupation: {ocupation},
-  aboutMe: {aboutMe},
-  links: {links}
-})
+import React, { useEffect, useState } from 'react';
+import styles from "./ProfileEdit.module.css";
+// import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import {getArtistById} from "../../redux/actions/getArtistById";
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { getArtistId } from '../../redux2/artistSlice';
 
 
+const ProfileEdit = () => {
 
-  return (
-    <div>
-      <button>
-        <img src={input.coverPhoto} alt='portada'/>
-      </button>
-      <div className='profile-foto-p'>
-        <button>
-          <img src={input.profilePhoto} alt='portada'/> 
-        </button>
-        <p>{name}</p>
-      </div>
+  const [errors, setErrors] = useState({});
+  const dispatch = useDispatch();
+  const usuario = useSelector(state => state.artist.usuario);
+  // console.log(usuario);
 
-    </div>
+  const id = 1;
+  useEffect(() => {
+   dispatch(getArtistId(id))
+  },[]);
+
+
+  const [input, setInput] = useState({
+    name: "",
+    lastname: "",
+    nickName: "",
+    profilePhoto: "",
+    coverPhoto: "", //Not here
+    email: "",
+    password: "",
+    city: "", //Not here
+    country: "", //Not here:
+    ocupation: "", //Not here:
+    aboutMe: "", //Not here:
+  });
+
+  function validate(input) {
+    let errors = {};
+    if (!input.name) {
+      errors.name = "Name is required";
+    }
+    return errors;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // if (!input.name) {
+    //   return alert("Name is required");
+    // }
+    // console.log(input);
+    axios
+      .put(`http://localhost:3001/artist/${id}`, input)
+      .then((res) => console.log(res))
+      .catch((errors) => errors);
+    // dispatch(postartist(input));
+    //alert(`Artist ${input.name} has been added`);
+    // setInput({
+    //   name: "",
+    //   password: "",
+    //   aboutMe: "",
+    // });
+  }
+
+  
+  function handleOnChange(e) {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+    // setErrors(
+    //   validate({
+    //     ...input,
+    //     [e.target.name]: e.target.value,
+    //   })
+    // );
+  }
+
+  return (<>
+    <form onSubmit={handleSubmit} className="form-container">
+            {/* <div className="form-container__left">
+              <label>
+                {rutaImagen ? (
+                  <img
+                    className="form-picture"
+                    src={rutaImagen}
+                    alt="Imagen de perfil"
+                  />
+                ) : (
+                  <AccountCircleIcon />
+                )}
+                <button
+                  className="upload-picture-button"
+                  type="button"
+                  onClick={handleClick}
+                >
+                  Subir foto
+                </button> 
+                 <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleFileChange}
+                  ref={fileInputRef}
+                  style={{ display: "none" }}
+                />
+                <br></br>
+                Registrate a<br></br> <b>Urban Club!</b>
+              </label>
+            </div> */}
+            <div className="form-container__middle">
+              <label className="required">
+                <div>
+                  <span style={{ color: "red" }}>*</span> Nombre:
+                </div>
+                <input
+                  onChange={handleOnChange}
+                  onBlur={handleOnChange}
+                  type="text"
+                  value={input.name}
+                  maxLength="35"
+                  name="name"
+                />
+              </label>
+              <label>
+                <div>
+                  <span style={{ color: "red" }}>*</span> Apellido:
+                </div>
+                <input
+                  type="text"
+                  value={input.lastname}
+                  onChange={handleOnChange}
+                  onBlur={handleOnChange}
+                  name="lastname"
+                  maxLength={35}
+                  required
+                />
+              </label>
+              <label>
+                <div>
+                  <span style={{ color: "red" }}>*</span> Correo:
+                </div>
+                <input
+                  type="email"
+                  value={input.email}
+                  onChange={handleOnChange}
+                  onBlur={handleOnChange}
+                  name="email"
+                  maxLength={45}
+                  required
+                />
+              </label>
+              <label>
+                <div>
+                  <span style={{ color: "red" }}>*</span> Nickname:
+                </div>
+                <input
+                  type="text"
+                  value={input.nickName}
+                  onChange={handleOnChange}
+                  onBlur={handleOnChange}
+                  name="nickName"
+                />
+              </label>
+              <label>
+                <div>
+                  <span style={{ color: "red" }}>*</span> Contraseña:
+                </div>
+                <input
+                  type="text"
+                  maxLength={45}
+                  value={input.password}
+                  onChange={handleOnChange}
+                  onBlur={handleOnChange}
+                  name="password"
+                  required
+                />
+              </label>
+            </div>
+            <div className="form-container__right">
+              <label>
+                <div>Ciudad:</div>
+                <input
+                  type="text"
+                  value={input.city}
+                  onChange={handleOnChange}
+                  onBlur={handleOnChange}
+                  name="city"
+                />
+              </label>
+              <label>
+                <div>Pais:</div>
+                <input
+                  type="text"
+                  value={input.country}
+                  onChange={handleOnChange}
+                  onBlur={handleOnChange}
+                  name="country"
+                />
+              </label>
+              <label>
+                <div>Ocupacion:</div>
+                <input
+                  type="text"
+                  value={input.ocupation}
+                  onChange={handleOnChange}
+                  onBlur={handleOnChange}
+                  maxLength={35}
+                  name="ocupation"
+                />
+              </label>
+              <label>
+                Descripción:
+                <textarea
+                  value={input.aboutMe}
+                  onChange={handleOnChange}
+                  onBlur={handleOnChange}
+                  maxLength={500}
+                  name="aboutMe"
+                />
+              </label>
+              <button className="upload-form-button" type="submit">
+                Save Changes
+              </button>
+            </div>
+          </form>
+  </>
   )
 }
 
