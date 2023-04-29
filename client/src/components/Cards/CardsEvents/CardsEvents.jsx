ed7bf68200629aa74134f1bb12f819d6dddc7450
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-04-29 20:55:31 -0300

    CardsEvents Añadidas a Home, Eventos creado, oscar

--- client/src/components/Cards/CardsEvents/CardsEvents.jsx
+++ client/src/components/Cards/CardsEvents/CardsEvents.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import style from "./CardsEvents.module.css";
 
 const CardsEvents = ({ events }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/events");
+  };
+
   return (
     <div className={style.container}>
       {events.map((event) => (
-        <div className={style.card} key={event.id}>
+        <div className={style.card} key={event.id} onClick={handleClick}>
           <div className={style.imageContainer}>
             <img src={event.image} alt={event.name} className={style.image} />
             <div className={style.overlay}>
