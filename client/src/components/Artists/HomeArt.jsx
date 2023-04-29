193d912dde641db90b3e3aaf7b91ddedaf65bdb3
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-04-29 11:20:31 -0300

    Registro y Perfil intacto, procedo a actualizar CSS oscar

--- client/src/components/Artists/HomeArt.jsx
+++ client/src/components/Artists/HomeArt.jsx
@@ -1,46 +1,37 @@
 import React from "react";
 import style from "./HomeArt.module.css";
 import TopArt from "./TopArt";
-import { getAllArts } from "../../redux2/artistSlice";
+import { getAllArts } from "../../redux/artistSlice";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect} from "react";
-
-
-
+import { useEffect } from "react";
 
 const HomeArt = () => {
-  const artista= useSelector(state => state.artist.allUsuarios)
+  const artista = useSelector((state) => state.artist.allUsuarios);
 
-  const dispatch=useDispatch();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getAllArts());
   }, []);
 
-  
-
   return (
-  <div className={style.ConteinerArt}>
-    
-    <div className={style.TopArt}>
-    {artista.map((item) => (
-      <div >  
-        <TopArt
-          key={item.index}
-          id={item.id}
-          name={item.name}
-          profilePhoto={item.profilePhoto}
-          ocupation={item.ocupation}
-          aboutMe={item.aboutMe}
-        />
-       </div>
-      ))}             
+    <div className={style.ConteinerArt}>
+      <div className={style.TopArt}>
+        {artista.map((item) => (
+          <div>
+            <TopArt
+              key={item.index}
+              id={item.id}
+              name={item.name}
+              profilePhoto={item.profilePhoto}
+              ocupation={item.ocupation}
+              aboutMe={item.aboutMe}
+            />
+          </div>
+        ))}
+      </div>
     </div>
-
-    
-
-  </div>
-  )
-}
+  );
+};
 
 export default HomeArt;
--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,39 +1,39 @@
-import './searchBar.css'
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { NavLink } from 'react-router-dom';
-import { getArtistName } from '../../redux2/artistSlice';
+import "./searchBar.css";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { NavLink } from "react-router-dom";
+import { getArtistName } from "../../redux/artistSlice";
 import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = () => {
-
   const dispatch = useDispatch();
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
 
   const handleOnChange = (e) => {
     e.preventDefault();
-    setName(e.target.value)
-  }
+    setName(e.target.value);
+  };
 
-    const handleOnClick = (e) => {
-      e.preventDefault();
-      if(name){
-        dispatch(getArtistName(name))
-        setName('')
+  const handleOnClick = (e) => {
+    e.preventDefault();
+    if (name) {
+      dispatch(getArtistName(name));
+      setName("");
     }
-  }
+  };
 
   return (
-    <div className='searchbar-container'>
+    <div className="searchbar-container">
       <form>
-        
-          <input className='searchbar'
-                 type='text'
-                 onChange={handleOnChange}
-                 placeholder='¿Qué artista quieres ver hoy?'/>
+        <input
+          className="searchbar"
+          type="text"
+          onChange={handleOnChange}
+          placeholder="¿Qué artista quieres ver hoy?"
+        />
 
-          <NavLink to={`/home?name=${name}`}>
-          <button className='searchbar-btn' onClick={e => handleOnClick(e)}>
+        <NavLink to={`/home?name=${name}`}>
+          <button className="searchbar-btn" onClick={(e) => handleOnClick(e)}>
             <i className="search-icon">
               <SearchIcon />
             </i>
@@ -41,7 +41,7 @@ const SearchBar = () => {
         </NavLink>
       </form>
     </div>
-  )
-}
+  );
+};
 
 export default SearchBar;
