857f491c6c3ad90cfe25c50a6e060c145e643b53
XCODE89 <maph0989@gmail.com>
2023-05-03 11:40:51 -0400

    Miguel - agregar mensaje de usuario inactivo - Mig_Dev

--- api/src/Controllers/artistControllers/getArtistInfo.js
+++ api/src/Controllers/artistControllers/getArtistInfo.js
@@ -1,20 +1,22 @@
 const generateJWT = require("../../../utils/generateJWT")
 const bcrypt = require("bcrypt")
 const {Artist} = require("../../db");
-const { ACTIVATED } = require("../../constants");
+const { ACTIVATED, DELETED } = require("../../constants");
 
 const getArtistInfo = async (email, password) => {
   console.log(email)
     const artist = await Artist.findOne({
         where: {
-          email,
-          estado:ACTIVATED
+          email
         }
       })
       
       if (!artist) {
-          throw new Error('No existe ningún usuario con este correo')
-        }   
+        throw new Error('No existe ningún usuario con este correo')
+      } 
+      if (artist.estado === DELETED) {
+        throw new Error('La cuenta esta inactiva')
+      } 
         //?usamos este metodos pars compars encriptaciones la password encriptada de bd con la password encriptada de login y asi evitar que se exponga la contraseña
         const comparePassword = bcrypt.compareSync(password, artist.password);
         if (comparePassword) {
