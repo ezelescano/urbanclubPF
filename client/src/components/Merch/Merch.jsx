19aaccbed3f0dbd0ce0aa039e365dd8038677d7b
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-05-04 04:07:42 -0300

    Progreso maquetado Tienda  css, oscar

--- client/src/components/Merch/Merch.jsx
+++ client/src/components/Merch/Merch.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import style from "./Merch.module.css";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import StarIcon from "@mui/icons-material/Star";
 
 const Merch = () => {
   return (
     <div className={style.containerBackground}>
       <div className={style.container}>
         <div className={style.row}>
-          <div className={style.boxNone}></div>
           <div className={style.productBanner}>
             <div className={style.productStar}>
               <img
@@ -14,14 +15,66 @@ const Merch = () => {
                 alt=""
               />
               <div className={style.productInfo}>
-                <span>Cuadro Decorativo Living</span>
+                <span className={style.productTitle}>
+                  <b>Cuadro Decorativo Living</b>
+                </span>
+                <br />
+                <span className={style.productSubtitle}>
+                  <span className={style.productRating}>
+                    <StarBorderIcon />
+                    <StarIcon />
+                    <StarIcon />
+                    <StarIcon />
+                    <StarIcon />
+                  </span>
+                  (200+ Reviews)
+                </span>
+                <div className={style.productDetails}>
+                  <span className={style.productDescription}>
+                    <p>Medidas:60 x 100 cm </p>
+                    <span className={style.productText}>
+                      Fabricado sobre madera mdf maciza de 1 cm de grosor (10
+                      mm) , ya listos para colgar con un sistema oculto para
+                      darle mejor estética (metal)
+                    </span>
+                  </span>
+                </div>
+                <div className={style.productPrice}>
+                  <span>Precio US$11.000</span>
+                </div>
+                <div className={style.productButtonSection}>
+                  <button className={style.productButton}>Comprar ahora</button>
+                  <button className={style.productButton}>
+                    Agregar al carrito
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div className={style.row}>
-          <div className={style.box2}></div>
-          <div className={style.box3}></div>
+        <div className={style.filterBar}>
+          <div className={style.leftBarContainer}>
+            <div className={style.filterContainer}>
+              <div>Filtros</div>
+              <ul className={style.filterList}>
+                <li>Todos</li>
+                <li>Precios Especiales</li>
+                <li>Basicos</li>
+              </ul>
+            </div>
+            <div className={style.categoryContainer}>
+              <div>Categorias</div>
+              <ul className={style.categoryList}>
+                <li>Música</li>
+                <li>Arte</li>
+                <li>Baile</li>
+                <li>Especiales</li>
+                <li>Exclusivos</li>
+                <li>Para eventos</li>
+                <li>Otros</li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
