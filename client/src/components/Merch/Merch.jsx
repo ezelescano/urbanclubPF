7cdeaa31a1fee1f7afba67d9034eccfe4526b096
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-05-04 02:50:15 -0300

    Primer intento maquetado tienda css

--- client/src/components/Merch/Merch.jsx
+++ client/src/components/Merch/Merch.jsx
@@ -5,10 +5,24 @@ const Merch = () => {
   return (
     <div className={style.containerBackground}>
       <div className={style.container}>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className={style.row}>
+          <div className={style.boxNone}></div>
+          <div className={style.productBanner}>
+            <div className={style.productStar}>
+              <img
+                src="https://res.cloudinary.com/dipn8zmq3/image/upload/v1683177467/UrbanClub/image_22_gqem3p.png"
+                alt=""
+              />
+              <div className={style.productInfo}>
+                <span>Cuadro Decorativo Living</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={style.row}>
+          <div className={style.box2}></div>
+          <div className={style.box3}></div>
+        </div>
       </div>
     </div>
   );
