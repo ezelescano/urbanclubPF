c813f626bd5d69d022f6fdd538773543daa7e71f
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-05-04 01:48:44 -0300

    Arreglos Fuentes y Tienda Mockup comenzado, oscar

--- client/src/components/Merch/Merch.jsx
+++ client/src/components/Merch/Merch.jsx
@@ -3,8 +3,13 @@ import style from "./Merch.module.css";
 
 const Merch = () => {
   return (
-    <div className={style.conteinerbackground}>
-      <div className={style.conteiner}></div>
+    <div className={style.containerBackground}>
+      <div className={style.container}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
