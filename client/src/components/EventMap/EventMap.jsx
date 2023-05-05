7326b9f388ce6aef90798b8625d02a8a286cb257
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-05-05 14:39:21 -0300

    Ya búsca por google maps automatico la geolocalización por nombre pero no es especifico, oscar

--- client/src/components/EventMap/EventMap.jsx
+++ client/src/components/EventMap/EventMap.jsx
@@ -2,11 +2,9 @@ import React, { useState, useEffect } from "react";
 
 function EventMap({ location }) {
   const [map, setMap] = useState(null);
-  const [place, setPlace] = useState(null);
 
   useEffect(() => {
     const googleMapsScript = document.createElement("script");
-    googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAY3zPk-r72ELJTbepL7koVfZ6XgrE63dY&libraries=places`; //Eso te envia Por LATITUD Y LONGITUD OJOOOO, No funciona por el momento los Places
     googleMapsScript.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyAY3zPk-r72ELJTbepL7koVfZ6XgrE63dY&libraries=places,geocode`;
     googleMapsScript.async = true;
     googleMapsScript.defer = true;
@@ -22,21 +20,26 @@ function EventMap({ location }) {
         mapOptions
       );
       setMap(mapInstance);
+
+      const geocoder = new window.google.maps.Geocoder();
+      geocoder.geocode({ address: "Tropitango" }, (results, status) => {
+        if (status === "OK") {
+          const location = results[0].geometry.location;
+          mapInstance.setCenter(location);
+          const marker = new window.google.maps.Marker({
+            position: location,
+            map: mapInstance,
+            title: "Tropitango",
+          });
+        } else {
+          console.error(
+            "Geocode was not successful for the following reason:",
+            status
+          );
+        }
+      });
     });
   }, [location]);
-  function geocodeAddress(address) {
-    const geocoder = new window.google.maps.Geocoder();
-    geocoder.geocode({ address: address }, (results, status) => {
-      if (status === "OK") {
-        setPlace(results[0].geometry.location);
-      } else {
-        console.error(
-          "Geocode was not successful for the following reason:",
-          status
-        );
-      }
-    });
-  }
 
   return <div id="map" style={{ height: "500px" }}></div>;
 }
