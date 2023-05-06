c98e5fabd8937f7edda2195e14ab60b5df8d42b0
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-05-06 05:48:43 -0300

    Migración de Login y Forgot password .css, oscar

--- client/src/components/ForgotPassword/ForgotPassword.jsx
+++ client/src/components/ForgotPassword/ForgotPassword.jsx
@@ -1,34 +1,38 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+//import style from "./ForgotPassword.module.css";
 import axios from "axios";
 
 const ForgotPassword = () => {
+  const [input, setInput] = useState({
+    email: "",
+  });
 
-    
-    const [input, setInput] = useState({
-        email: "",
+  const changeHandler = (e) => {
+    setInput({
+      email: e.target.value,
     });
+  };
 
-    const changeHandler = (e) => {
-        setInput({
-            email: e.target.value,
-          });
-    }
+  const submitHandler = (e) => {
+    e.preventDefault();
+    axios.put("http://localhost:3001/artist/forgotPassword", input);
+  };
 
-    const submitHandler = (e) => {
-        e.preventDefault();
-        axios.put("http://localhost:3001/artist/forgotPassword", input);
-    }
-
-    return (
-        <div>
-            <form onSubmit={submitHandler}>
-            <label>Ingresa tu correo electrónico</label>
-            <input type="text" name="email" value={input.email} onChange={changeHandler}></input>
-            <button type="submit">Recuperar contraseña</button>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <label>Ingresa tu correo electrónico</label>
+        <input
+          type="text"
+          name="email"
+          value={input.email}
+          onChange={changeHandler}
+        ></input>
+        <button type="submit">Recuperar contraseña</button>
+      </form>
+    </div>
+  );
+};
 
 export default ForgotPassword;
