22e070ddbdd277cab49a7ee556e9d6d0477d8a72
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-05-06 06:11:37 -0300

    Settings  Migrado module.css, oscar

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -1,21 +1,33 @@
-import "./settings.css";
-import React from 'react'
-import { NavLink, } from 'react-router-dom'
+import style from "./settings.module.css";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-function Settings({handleDeleteAccount, handleLogout, handleShowEdit, handlePasswordChange}) {
-  
+function Settings({
+  handleDeleteAccount,
+  handleLogout,
+  handleShowEdit,
+  handlePasswordChange,
+}) {
   return (
-    <div className='settings-container'>
+    <div className={style.settingsContainer}>
       {/* <NavLink className='navlink-st' to='/ProfileEdit'>
       <div className="div-settings">Editar perfil</div>
       </NavLink> */}
-      <div className="div-settings"><button onClick={handleShowEdit}>Editar perfil</button></div>
-      <div className="div-settings"><button onClick={handleLogout}>Logout</button></div>
-      <div className="div-settings"><button onClick={handlePasswordChange}>Cambiar contraseña</button></div>
+      <div className={style.divSettings}>
+        <button onClick={handleShowEdit}>Editar perfil</button>
+      </div>
+      <div className={style.divSettings}>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+      <div className={style.divSettings}>
+        <button onClick={handlePasswordChange}>Cambiar contraseña</button>
+      </div>
       <NavLink to="/updatePassword/:id" />
-      <div className="div-settings-d"><button onClick={handleDeleteAccount}>Eliminar Cuenta</button></div>
+      <div className={style.divSettingsD}>
+        <button onClick={handleDeleteAccount}>Eliminar Cuenta</button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Settings
+export default Settings;
