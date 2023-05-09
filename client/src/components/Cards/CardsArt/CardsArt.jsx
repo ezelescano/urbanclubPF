ba6074745b2002d99ff1bc4a1ce2e7284a560f5b
ppalmav <ppalmav@users.noreply.github.com>
2023-05-09 18:15:05 -0400

    Pablo_Dev: Fixes crear evento, Tags a ocupaciones en CardArt

--- client/src/components/Cards/CardsArt/CardsArt.jsx
+++ client/src/components/Cards/CardsArt/CardsArt.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import style from "./CardsArt.module.css";
 import { NavLink } from "react-router-dom";
+import OcupationTag from "./OcupationTag";
 
 const CardsArt = ({ name, profilePhoto, ocupation, aboutMe, id, Events }) => {
+  
+  const ocupationRows = ocupation.split(",").map((ocupation) => 
+  {
+    return <OcupationTag
+    ocupation={ocupation}
+      key={ocupation + id} />
+    });
+
+  // ocupation.split(",").map((ocup) => {
+  //   ocupationRows.push(
+  //     <OcupationTag
+  //       ocupation={ocup}
+  //       key={id} />
+  //   );
+
+  // });
+
+  
   return (
     <NavLink className={style.Link} to={`/profile/${id}`}>
       <div className={style.Container}>
         <img className={style.image} src={profilePhoto} alt={name} />
         <div className={style.text}>
           <h3>{name}</h3>
-          <h5>Occupation:</h5>
-          <p>{ocupation}</p>
-          <h5>About me:</h5>
+          <h5>Ocupación:</h5>
+          <div>{ocupationRows}</div>
+          <h5>Sobre mí:</h5>
           <p>{aboutMe}</p>
         </div>
       </div>
