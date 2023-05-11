3dbaf47da5267f92c78e635e84db16218e4ed0de
Alan Winkler <alanquimeywinkler@gmail.com>
2023-05-11 02:10:57 -0300

    Alan_Dev términos y condiciones

--- client/src/components/TermsAndConditions/TermsAndConditions.jsx
+++ client/src/components/TermsAndConditions/TermsAndConditions.jsx
@@ -17,23 +17,23 @@ const TermsAndConditions = () => {
         <h2>2. Registro y Cuenta de Usuario</h2>
         <h1>
           Al registrarte en nuestro sitio web, aceptas proporcionar información
-          precisa y actualizada. Eres responsable de mantener la confidencialidad
-          de tu cuenta y contraseña, y aceptas ser responsable de todas las
-          actividades que ocurran bajo tu cuenta.
+          precisa y actualizada. Eres responsable de mantener la
+          confidencialidad de tu cuenta y contraseña, y aceptas ser responsable
+          de todas las actividades que ocurran bajo tu cuenta.
         </h1>
 
         <h2>3. Propiedad Intelectual</h2>
         <h1>
           Este sitio web y su contenido son propiedad nuestra o de nuestros
-          licenciantes. Queda prohibida la reproducción, distribución, exhibición,
-          transmisión o cualquier otro uso no autorizado del contenido sin nuestro
-          permiso previo por escrito.
+          licenciantes. Queda prohibida la reproducción, distribución,
+          exhibición, transmisión o cualquier otro uso no autorizado del
+          contenido sin nuestro permiso previo por escrito.
         </h1>
 
         <h2>4. Enlaces a Terceros</h2>
         <h1>
-          Nuestro sitio web puede incluir enlaces a sitios web de terceros. Estos
-          enlaces se proporcionan para tu conveniencia y para proporcionar
+          Nuestro sitio web puede incluir enlaces a sitios web de terceros.
+          Estos enlaces se proporcionan para tu conveniencia y para proporcionar
           información adicional. No tenemos control sobre el contenido de los
           sitios web de terceros y no somos responsables de su contenido. El uso
           de cualquier sitio web enlazado es bajo tu propio riesgo.
@@ -43,15 +43,16 @@ const TermsAndConditions = () => {
         <p>
           En la medida máxima permitida por la ley, no seremos responsables por
           cualquier pérdida o daño directo, indirecto, incidental, consecuente o
-          especial que resulte del uso o la imposibilidad de uso de este sitio web.
+          especial que resulte del uso o la imposibilidad de uso de este sitio
+          web.
         </p>
 
         <h2>6. Ley Aplicable</h2>
         <h1>
           Estos términos y condiciones se regirán e interpretarán de acuerdo con
           las leyes vigentes. Cualquier disputa relacionada con estos términos y
-          condiciones estará sujeta a la jurisdicción exclusiva de los tribunales
-          competentes.
+          condiciones estará sujeta a la jurisdicción exclusiva de los
+          tribunales competentes.
         </h1>
       </div>
     </div>
