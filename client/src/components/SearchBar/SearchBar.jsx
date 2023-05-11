1c19e7fc727d89c2e8da5573ba636e4b0854ea1c
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-05-11 05:41:55 -0300

    Navbar Responsive, de nuevo  oscar

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,41 +1,58 @@
-import "./searchBar.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { getArtistName } from "../../redux/artistSlice";
 import SearchIcon from "@mui/icons-material/Search";
-import {  useNavigate, useLocation } from "react-router-dom";
-
+import { useNavigate, useLocation } from "react-router-dom";
+import "./searchBar.css";
 
 const SearchBar = () => {
   const dispatch = useDispatch();
   const location = useLocation();
   const navigate = useNavigate();
   const [input, setInput] = useState({
-    buscar: ''
-})
+    buscar: "",
+  });
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   const handleOnChange = (e) => {
     e.preventDefault();
     setInput({
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   const handleOnClick = async (e) => {
     e.preventDefault();
     if (input.buscar) {
-      if(location.pathname.split("/").pop() !== 'artists'){ 
-        //si location no es artist, cambiar a artists y generar busqueda
-        await navigate("/artists")
+      if (location.pathname.split("/").pop() !== "artists") {
+        // si location no es artist, cambiar a artists y generar busqueda
+        await navigate("/artists");
       }
       dispatch(getArtistName(input.buscar));
       setInput({
-        buscar:''
+        buscar: "",
       });
-      
     }
   };
+
+  const placeholderText =
+    windowWidth <= 1000
+      ? "¿Algún artista en mente?"
+      : "¿Qué artista quieres ver hoy?";
+      
 
   return (
     <div className="searchbar-container">
@@ -46,7 +63,7 @@ const SearchBar = () => {
           name="buscar"
           onChange={handleOnChange}
           value={input.buscar}
-          placeholder="¿Qué artista quieres ver hoy?"
+          placeholder={placeholderText}
           autoComplete="off"
         />
         <NavLink to={`/home?name=${input.buscar}`}>
