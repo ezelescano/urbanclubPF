a6244f7d94b009f6609673ac993e51797044de45
AlhuayOscar <90626121+AlhuayOscar@users.noreply.github.com>
2023-05-13 20:37:28 -0300

    Artists WIP 25% css, oscar

--- client/src/components/Cards/CardsArt/CardsArt.jsx
+++ client/src/components/Cards/CardsArt/CardsArt.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./CardsArt.module.css";
 import { NavLink } from "react-router-dom";
 import OcupationTag from "./OcupationTag";
@@ -19,24 +19,28 @@ const CardsArt = ({
       .map((ocupation) => (
         <OcupationTag ocupation={ocupation} key={ocupation + id} />
       ));
+  /* Overlay letra por letra: */
+  const [isOverlayVisible, setIsOverlayVisible] = useState(false);
+  const coverPhoto =
+    "https://images.unsplash.com/photo-1504805572947-34fad45aed93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y292ZXIlMjBwaG90b3xlbnwwfHwwfHw%3D&w=1000&q=80";
+  const handleMouseEnter = () => {
+    setIsOverlayVisible(true);
+  };
 
-  // ocupation.split(",").map((ocup) => {
-  //   ocupationRows.push(
-  //     <OcupationTag
-  //       ocupation={ocup}
-  //       key={id} />
-  //   );
-
-  // });
-
+  const handleMouseLeave = () => {
+    setIsOverlayVisible(false);
+  };
   return (
     <NavLink className={style.Link} to={`/profile/${id}`}>
-      <div className={style.Container}>
-        <img className={style.image} src={profilePhoto} alt={name} />
-        <div className={style.text}>
+      <div className={`${style.Container} ${style.overlayContainer}`}>
+        <div className={style.images}>
+          <img className={style.userCover} src={coverPhoto} alt={name} />
+          <img className={style.userImage} src={profilePhoto} alt={name} />
+        </div>
+        <div className={`${style.text} ${style.overlay}`}>
           <h3>{name}</h3>
           <h5>Ocupación:</h5>
-          <div>{ocupationRows}</div>
+          <div className={`${style.fadeInText}`}>{ocupationRows}</div>
           {/* <h5>Sobre mí:</h5>
           <p>{aboutMe}</p> */}
           {/* <h5>Ubicacion:</h5> */}
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -11,72 +11,55 @@ function Navbar() {
   const usuario = useSelector((state) => state.auth.user);
 
   const [navLinksVisible, setNavLinksVisible] = useState(false);
-
+  const [navbarHeight, setNavbarHeight] = useState(0);
   const handleHamburgerClick = () => {
     setNavLinksVisible(!navLinksVisible);
   };
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      if (scrollTop > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+    const navbarElement = document.getElementById("navbar");
+    if (navbarElement) {
+      setNavbarHeight(navbarElement.offsetHeight);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <nav className={style.navbar}>
-      <NavLink to="/">
-        <img
-          className={style.navTitleImg}
-          src="https://res.cloudinary.com/dipn8zmq3/image/upload/v1683969976/UrbanClub/our_logo-removebg-preview_auto_x2-removebg-preview_1_tsbanf.png"
-          alt=""
-        />
-        <img
-          className={style.navTitleImgZoom}
-          src="https://res.cloudinary.com/dipn8zmq3/image/upload/v1683969976/UrbanClub/our_logo-removebg-preview_auto_x2-removebg-preview_1_tsbanf.png"
-          alt=""
-        />
-      </NavLink>
-      <div className={style.searchbarWrapper}>
-        <SearchBar />
-      </div>
-      {/*<SearchIcon style={{color:"white"}}/>*/}
-      <ul className={style.navLinks}>
-        <li>
-          <NavLink to="/admin" className={`${style.navLink} ${style.active}`}>
-            Admin
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/artists" className={`${style.navLink} ${style.active}`}>
-            Artistas
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/aboutus" className={`${style.navLink} ${style.active}`}>
-            Nosotros
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/events" className={`${style.navLink} ${style.active}`}>
-            Eventos
-          </NavLink>
-        </li>
-        {/* <li>  // ! Comentada la tienda
-          <NavLink to="/merch" className={`${style.navLink} ${style.active}`}>
-            Tienda
-          </NavLink>
-        </li> */}
-        {islogin.isAuthenticated && (
+    <>
+      <nav className={`${style.navbar} ${isScrolled ? style.scrolled : ""}`}>
+        <NavLink to="/">
+          <img
+            className={style.navTitleImg}
+            src="https://res.cloudinary.com/dipn8zmq3/image/upload/v1683969976/UrbanClub/our_logo-removebg-preview_auto_x2-removebg-preview_1_tsbanf.png"
+            alt=""
+          />
+          <img
+            className={style.navTitleImgZoom}
+            src="https://res.cloudinary.com/dipn8zmq3/image/upload/v1683969976/UrbanClub/our_logo-removebg-preview_auto_x2-removebg-preview_1_tsbanf.png"
+            alt=""
+          />
+        </NavLink>
+        <div className={style.searchbarWrapper}>
+          <SearchBar />
+        </div>
+        {/*<SearchIcon style={{color:"white"}}/>*/}
+        <ul className={style.navLinks}>
           <li>
-            <NavLink to="/messenger" className="nav-link active">
-              Chat
+            <NavLink to="/admin" className={`${style.navLink} ${style.active}`}>
+              Admin
             </NavLink>
           </li>
-        )}
-      </ul>
-      <div className={style.burgerSelect}>
-        <button className={style.hamburgerBtn} onClick={handleHamburgerClick}>
-          ☰
-        </button>
-        <ul
-          className={`${style.respNavLinks} ${
-            navLinksVisible ? style.slideIn : style.slideOut
-          }`}
-          id="navLinks"
-        >
           <li>
             <NavLink
               to="/artists"
@@ -90,7 +73,7 @@ function Navbar() {
               to="/aboutus"
               className={`${style.navLink} ${style.active}`}
             >
-              Acerca de
+              Nosotros
             </NavLink>
           </li>
           <li>
@@ -102,41 +85,90 @@ function Navbar() {
             </NavLink>
           </li>
           {/* <li>  // ! Comentada la tienda
+          <NavLink to="/merch" className={`${style.navLink} ${style.active}`}>
+            Tienda
+          </NavLink>
+        </li> */}
+          {islogin.isAuthenticated && (
+            <li>
+              <NavLink to="/messenger" className="nav-link active">
+                Chat
+              </NavLink>
+            </li>
+          )}
+        </ul>
+        <div className={style.burgerSelect}>
+          <button className={style.hamburgerBtn} onClick={handleHamburgerClick}>
+            ☰
+          </button>
+          <ul
+            className={`${style.respNavLinks} ${
+              navLinksVisible ? style.slideIn : style.slideOut
+            }`}
+            id="navLinks"
+          >
+            <li>
+              <NavLink
+                to="/artists"
+                className={`${style.navLink} ${style.active}`}
+              >
+                Artistas
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/aboutus"
+                className={`${style.navLink} ${style.active}`}
+              >
+                Acerca de
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/events"
+                className={`${style.navLink} ${style.active}`}
+              >
+                Eventos
+              </NavLink>
+            </li>
+            {/* <li>  // ! Comentada la tienda
             <NavLink to="/merch" className={`${style.navLink} ${style.active}`}>
               Tienda
             </NavLink>
           </li> */}{" "}
+            {!islogin.isAuthenticated ? (
+              <NavLink to="/login">
+                <button className={style.navLoginBtnZoom}>Ingresar</button>
+              </NavLink>
+            ) : (
+              <NavLink to={`/profile/${islogin.user.id}`}>
+                <img
+                  className={style.sesionfotozoom}
+                  src={usuario.profilePhoto}
+                  alt="No hay"
+                />
+              </NavLink>
+            )}
+          </ul>
+        </div>
+        <div className="profilePicture">
           {!islogin.isAuthenticated ? (
             <NavLink to="/login">
-              <button className={style.navLoginBtnZoom}>Ingresar</button>
+              <button className={style.navLoginBtn}>Ingresar</button>
             </NavLink>
           ) : (
             <NavLink to={`/profile/${islogin.user.id}`}>
               <img
-                className={style.sesionfotozoom}
+                className={style.sesionfoto}
                 src={usuario.profilePhoto}
                 alt="No hay"
               />
             </NavLink>
           )}
-        </ul>
-      </div>
-      <div className="profilePicture">
-        {!islogin.isAuthenticated ? (
-          <NavLink to="/login">
-            <button className={style.navLoginBtn}>Ingresar</button>
-          </NavLink>
-        ) : (
-          <NavLink to={`/profile/${islogin.user.id}`}>
-            <img
-              className={style.sesionfoto}
-              src={usuario.profilePhoto}
-              alt="No hay"
-            />
-          </NavLink>
-        )}
-      </div>
-    </nav>
+        </div>
+      </nav>
+      <div style={{ height: navbarHeight, backgroundColor: "#000000" }}></div>
+    </>
   );
 }
 
