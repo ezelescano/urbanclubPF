3d29765f06314365f7c6d063397752a85b95ebbb
Alan Winkler <alanquimeywinkler@gmail.com>
2023-05-13 23:22:20 -0300

    Alan_Dev comentarios agregado

--- api/src/Handlers/comentsHandler/getAllComentsHandler.js
+++ api/src/Handlers/comentsHandler/getAllComentsHandler.js
@@ -1,9 +1,12 @@
 const getAllComments = require("../../Controllers/comentsController/getAllComments");
 
 const getAllComentsHandler = async (req, res) => {
-    const {id_event} = req.body
+    const {idEvent} = req.params;
     try {
-        const comments = await getAllComments(id_event);
+        const comments = await getAllComments(idEvent);
+        if(!comments) {
+            return res.status(400).json({error: "No comments founds for this event"})
+        }
         return res.status(200).json(comments);
     } catch (error) {
         return res.status(400).json({error: "No comments founds in database"})
--- api/src/routes/eventCommentRouter.js
+++ api/src/routes/eventCommentRouter.js
@@ -3,7 +3,7 @@ const eventCommentRouter = Router();
 const getAllComentsHandler = require("../Handlers/comentsHandler/getAllComentsHandler");
 const postCommentHandler = require("../Handlers/comentsHandler/postCommentHandler");
 
-eventCommentRouter.get("/", getAllComentsHandler);
+eventCommentRouter.get("/:idEvent", getAllComentsHandler);
 eventCommentRouter.post("/", postCommentHandler);
 
 module.exports = eventCommentRouter;
