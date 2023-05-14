b19b02b51d43674e5bf57f654259caa0a91ec26c
estiven2111 <estiven.a.martinez@gmail.com>
2023-05-13 21:59:08 -0500

    event

--- client/src/components/ComponentEvents/DetailsEvents/DetailsEvents.jsx
+++ client/src/components/ComponentEvents/DetailsEvents/DetailsEvents.jsx
@@ -122,50 +122,50 @@ function DetailsEvents() {
         const buy = await dispatch(buyTicket(compraticket));
         let timer = null;
         console.log(buy);
-    const urlPay = buy.link
-   
-    // const googleLoginURL = "https://pruebaback-production-0050.up.railway.app/artist/auth/google"
-    const newWindow = window.open(urlPay,"_blank","width=550,height=550")
-console.log(newWindow);
-let eventd
-eventd = await axios.put(
-  `/events/buyTicket/${detailEvent.id}`,
-  // `https://pruebaback-production-0050.up.railway.app/events/buyTicket/${detailEvent.id}`,
-  stockObjeto
-); 
+        const urlPay = buy.link
 
-  
+        // const googleLoginURL = "https://pruebaback-production-0050.up.railway.app/artist/auth/google"
+        const newWindow = window.open(urlPay, "_blank", "width=550,height=550")
+        console.log(newWindow);
+        let eventd
+        eventd = await axios.put(
+          `/events/buyTicket/${detailEvent.id}`,
+          // `https://pruebaback-production-0050.up.railway.app/events/buyTicket/${detailEvent.id}`,
+          stockObjeto
+        );
 
 
-    if(newWindow) {
-        timer = setInterval(async() => {
-            if(newWindow.closed) {
-             if(timer) clearInterval(timer)
-console.log("prueba");
-             if (eventd) {
-              swal({
-                title: "COMPRA EXITOSA",
-                text: `Revisa tu correo para ver más detalles de la compra`,
-                icon: "success",
-                buttons: "Aceptar",
-              });
-             }else{
-              swal({
-                title: "COMPRA EXITOSA",
-                text: `tuvimos problemas enviando el email a tu correo consulta con urbanclub!!!`,
-                icon: "info",
-                buttons: "Aceptar",
-              });
-             }
 
 
-            } 
-        }, 500)
-        
-    }
-       
+        if (newWindow) {
+          timer = setInterval(async () => {
+            if (newWindow.closed) {
+              if (timer) clearInterval(timer)
+              console.log("prueba");
+              if (eventd) {
+                swal({
+                  title: "COMPRA EXITOSA",
+                  text: `Revisa tu correo para ver más detalles de la compra`,
+                  icon: "success",
+                  buttons: "Aceptar",
+                });
+              } else {
+                swal({
+                  title: "COMPRA EXITOSA",
+                  text: `tuvimos problemas enviando el email a tu correo consulta con urbanclub!!!`,
+                  icon: "info",
+                  buttons: "Aceptar",
+                });
+              }
 
-        
+
+            }
+          }, 500)
+
+        }
+
+
+
       } else {
         swal({
           title: "ENTRADAS AGOTADAS",
@@ -183,19 +183,33 @@ console.log("prueba");
 
   return (
     <>
-      <div className={style.backContainer}>
+       <div className={style.backContainer}>
         <div className={style.container}>
-          {/* Resto del código */}
+          <br></br>
+          <div className={style.img_Es}>
+            <img src={detailEvent.eventPhoto} alt="" />
+          </div>
           {islogin.user.id !== detailEvent.id_Artist ? (
             <div className={style.Description}>
-              {/* Resto del código */}
+              <h1>{detailEvent.name}</h1>
+              <br />
+              <h5>{detailEvent.nameArena}</h5>
+              <h5>{detailEvent.location}</h5>
+              <h5>
+                <CalendarMonthIcon style={{ fontSize: "12px" }} />{" "}
+                {detailEvent.date}
+              </h5>
+              <h4>PRECIO</h4>
+              <h5> U$S {detailEvent.price}</h5>
+              <h4>Descripcion</h4>
+              <p>{detailEvent.Description}</p>
+              <br />
               <label htmlFor="">¿Cual es tú país?</label>
               <input
                 type="text"
                 name="Country"
                 value={destino.Country}
                 onChange={getdestinohandler}
-                className={style.roundedInput}
               />
               <label htmlFor="">¿Y tu ciudad?</label>
               <input
@@ -203,16 +217,70 @@ console.log("prueba");
                 name="city"
                 value={destino.city}
                 onChange={getdestinohandler}
-                className={style.roundedInput}
               />
-              {/* Resto del código */}
+              <button disabled={destino.ban} onClick={ubicationHandler}>
+                Cómo llego ahi?
+              </button>
+              <button disabled={!destino.ban} onClick={resetUbicationHandler}>
+                Reinicar la Busqueda
+              </button>
+              <div className={style.links}>
+                <br />
+                <h3>Cantidad de entradas Disponibles</h3>
+                <h3>{cantidad}</h3>
+                <div>
+                  <label>Comprar Entradas con Debito o Crédito:</label>
+                  <br />
+                  <input
+                    name="entradas"
+                    value={entradas}
+                    min="1"
+                    type="number"
+                    onChange={handleOnChange}
+                  />
+                </div>
+                <button onClick={buyTicketHandler}>
+                  Comprar entrada <CreditCardIcon />
+                </button>
+                <p>Total a pagar: {entradas * event.price} USD</p>
+                <a
+                  href="https://www.visa.com.ar"
+                  target="_blank"
+                  rel="noreferrer"
+                ></a>
+                <br />
+                <a
+                  href="https://www.google.com/maps/place/Teatro+Gran+Rex/@-34.6033873,-58.5313019,12z/data=!4m10!1m2!2m1!1sGran+Rex!3m6!1s0x95bccaceed5746b9:0xf933ab84305babc0!8m2!3d-34.6033873!4d-58.3788666!15sCghHcmFuIFJleJIBF3BlcmZvcm1pbmdfYXJ0c190aGVhdGVy4AEA!16s%2Fm%2F05bzpqm"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button>
+                    Compralo desde Teatro Gran Rex <StoreMallDirectoryIcon />
+                  </button>
+                </a>
+              </div>
+              <br></br>
             </div>
           ) : islogin.isAuthenticated ? (
             <UpdateEvents id={id} event={detailEvent} />
           ) : (
             <div></div>
           )}
-          {/* Resto del código */}
+          <br></br>
+          <br></br>
+          <br></br>
+          {destino.ban === true ? (
+            <div className={style.maps}>
+              <Maps
+                city={detailEvent.Country}
+                country={detailEvent.city}
+                Dcity={destino.city}
+                Dcountry={destino.country}
+              />
+            </div>
+          ) : (
+            <div></div>
+          )}
         </div>
       </div>
     </>
